5ce9f429e43b70387923b0a8ffc58c591e072802
Nathalie Pillen <56022155+NathaliePillen@users.noreply.github.com>
2020-01-07 16:04:28 +0100

    update footer

---
+++ view/footer.php
@@ -0,0 +1,8 @@
+    
+    <footer class="section">
+        <div class="container-fluid p-3 mb-2 bg-secondary text-white copyright text-center">
+        Copyright &copy; 2019 <span> KANE Projects</span>
+        </div>
+   </footer>
+    
+</body>

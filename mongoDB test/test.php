ad29245811ca2ecde2727dd0be466083f5063a70
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-08 14:19:10 +0100

    test read everything works. also created a function that counts number of documents in a collection

--- mongoDB test/test.php
+++ mongoDB test/test.php
@@ -13,15 +13,18 @@
         $username = "KasraTabrizi";
         $password = "codebook";
         $collections = array("users" => "Users", "skills" => "Skills", "project" => "Projects");
-
-        $users = array("email" => "test", "password" => "test", "first name" => "test", "last name" => "test", "Job Title" => "test","Profile Image" => "test");
-
-        $skill_names = array("HTML", "CSS", "Bootstrap", "TailWind", "Bulma", "Sass", 
+        $usersKey = array("email", "password", "first_name", "last_name", "job_title", "github", "profile_image", "skills", "collaborators", "projects");
+        $skillsKey = array("skill", "icon-image");
+        $skillName = array("HTML", "CSS", "Bootstrap", "TailWind", "Bulma", "Sass", 
                         "PHP", "MySQL", "MongoDB", "Javascript", "Typescript", "Vue.js", 
                         "React", "Angular", "Three.js", "OpenGL", "SVG", "Node.js", 
                         "Laravel", "Express.js");
-        
+        $skillIconImage = array();
+        $projectsKey = array("title", "description", "github", "skills", "number_of_collaborators", "collaborators");
 
+        $users = array("email" => "test", "password" => "test", "first name" => "test", "last name" => "test", "Job Title" => "test","Profile Image" => "test");
+
+        //CONNECT TO THE DATABASE
         function connectDB($username, $password){
             $database = new MongoDB\Client("mongodb+srv://".$username.":".$password."@codebook-cqlfl.mongodb.net/test?retryWrites=true&w=majority");
             return $database;
@@ -56,22 +59,36 @@
 
         //DELETE A DOCUMENT FROM A COLLECTION
         function deleteDB($database,$collectionName,$data){
-
+            $collection = $database->CodeBook->$collectionName;
+            $result = $collection->deleteOne($data);
+            return $result;
         }
 
+        //
+        function collectionLength($database,$collectionName){
+            $collection = $database->CodeBook->$collectionName;
+            return $collection->count();
+        }
+
+        //TEST CONNECTION TO DATABASE
         $database = connectDB($username, $password);
-        $collection = $database->CodeBook->Users;
-        $result = $collection->find();
-        var_dump($collection->count());
+
+        //TEST INSERT
+
+        //TEST READ ONE DOCUMENT
+
+        //TEST READ EVERYTHING
+        $result = readCollection($database, $collections['users']);
+        var_dump(collectionLength($database, $collections['users']));
         foreach($result as $item){
             var_dump($item["_id"]);
         }
-        // $collection = $client->CodeBook->Skills;
-        // foreach ($skills as $skill){
-        //     $oneSkill = array("skill" => $skill);
-        //     var_dump($oneSkill);
-        //     $result = $collection->insertOne($oneSkill);
-        // }
+
+        //UPDATE ONE DOCUMENT
+
+
+        //DELETE ONE DOCUMENT
+
     ?>
 </body>
 </html>

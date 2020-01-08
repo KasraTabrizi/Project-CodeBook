b3c063d21360c72dc64aee4efc029e8f50e62472
Nathalie Pillen <56022155+NathaliePillen@users.noreply.github.com>
2020-01-08 10:26:06 +0100

    include styles-scripts

--- view/index.php
+++ view/index.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    
+    <?php include('styles.php'); ?>
 </head>
 <body>
 
@@ -14,8 +13,6 @@
 
 
 <?php include('footer.php'); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<?php include('scripts.php'); ?>
 </body>
 </html>

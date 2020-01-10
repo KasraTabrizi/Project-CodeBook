5676b1769848e69ab61dd92b1e523a3a9cb7b47e
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-10 14:09:42 +0100

    changes

--- mongoDB test/profile_generator.php
+++ mongoDB test/profile_generator.php
@@ -8,21 +8,24 @@ generateUsers();
 function generateUsers()
 {
     // Edit this info to make different random variables
+    $data_users;
+    $data_projects;
+    //user variables
     $first_name = array("Geert", "Reinaert", "Sander", "Stijn", "Cath", "Claas", "David", "Jan", "Kristel", "Moumita", "Naoyuki", "Tibo", "Welid", "Adel", "Aline", "Crisse", "Danny", "Thijs", "Kevin", "Erin", "Jasper", "Jeroen", "Kasra", "Lisa", "Myle", "Nathalie", "Rico", "Shadia", "Steven", "Tabitha");
     $last_name = array("Uyttendael", "Geeraerts", "Seeuws", "Depourcq", "Coleman", "Mulemaster", "Steigschmetterling", "The Clerk", "Von Mile Gem", "Basak", "Arakawa", "Labidi", "Lallaloui", "Longnose", "Sotto", "Joosen Louise-Hartley", "Springer", "De Schoenveter", "Tabrizionales", "Cant", "Truong", "Pillpopper", "Hangkok", "Nasman", "Notens", "Bidee");
     $password = "dummy";
     $github = "https://github.com";
     $job_title = array("Junior Front End Developer", "Senior Front End Developer", "Junior Back End Developer", "Senior Back End Developer","Junior Full Stack Developer","Senior Full Stack Developer","Dev Ops","UI Designer","UX Designer");
-    //$email = array("@kane.com", "@fmail.com", "@jbidy.com", "@tlam.com", "@msn.be", "@jollywolly.com");
     $email = array("@kane.com");
     $profile_image = "https://www.cornwallbusinessawards.co.uk/wp-content/uploads/2017/11/dummy450x450.jpg";
     $created_at = date('Y-m-d');
-    $data_users;
-    $data_projects;
     $users_skills = array();
+
+    //project variables
     $project_title = array("To Do List", "Tetris", "Weather API", "Uber Clone", "Instagram Clone", "Music API", "SnapChat", "AI face recognition", "Robotics", "Poker Game", "3D printer API", "Online RPG Game", "Arduino API", "Full Stack Framework");
     $project_description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     $project_skills = array();
+    $project_founder;
     $num_collabs = array(2,3,4,5);
     $collaborators_id = array();
     $user_ids = array();
@@ -35,19 +38,14 @@ function generateUsers()
 
     //generate users
     for ($i = 0; $i < 5; $i++) {
-        $fname = randomGen($first_name);
-        $lname = randomGen($last_name);
-        $jtitle = randomGen($job_title);
-        //$mail = $fname . randomGen($email);
-        $encryptPassword = password_hash($password, PASSWORD_DEFAULT);
-
         $data_users['email'] = $mail;
-        $data_users['password'] = $encryptPassword;
-        $data_users['first_name'] = $fname;
-        $data_users['last_name'] = $lname;
-        $data_users['job_title'] = $jtitle ;
+        $data_users['password'] = password_hash($password, PASSWORD_DEFAULT);
+        $data_users['first_name'] = randomGen($first_name);
+        $data_users['last_name'] = randomGen($last_name);
+        $data_users['job_title'] = randomGen($job_title);
         $data_users['github'] = $github;
         $data_users['profile_image'] = $profile_image;
+        //generate skills
         for($i = 0; $i < 5; $i++){
             $randomId = generateSkills($database);
             if(checkDuplicateSkill($users_skills, $randomId)){
@@ -58,11 +56,12 @@ function generateUsers()
         $data_users['collaborators'] = array();
         $data_users['projects'] = array();
         $data_users['created_at'] = $created_at;
-
+        //Add to Database
         $user_id_new = createOneDB($database, "Users", $data_users);
+        //add the user id to an array for later use in projects
         array_push($user_ids, getIdString($user_id_new));
-        //var_dump($user_ids);
     }
+
     //generate projects
     for($i = 0; $i < 10; $i++){
         $ptitle = randomGen($project_title);
@@ -71,6 +70,7 @@ function generateUsers()
         $data_projects['title'] = $ptitle;
         $data_projects['description'] = $project_description;
         $data_projects['github'] = $github;
+        //generate skills
         for($i = 0; $i < 5; $i++){
             $randomId = generateSkills($database);
             if(checkDuplicateSkill($project_skills, $randomId)){
@@ -101,7 +101,6 @@ function generateSkills($database){
         return getIdString($item["_id"]);
     }
 }
-
 //function that checks if the skill already occurs in the skills list of a user or project
 function checkDuplicateSkill($array, $skillId){
     foreach($array as $item){

4867a401210331181be3c87d64a1af7302b80337
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-10 12:16:40 +0100

    addCollaborator function

--- model/model.php
+++ model/model.php
@@ -2,10 +2,13 @@
 
 //FUNCTION THAT GENERATES A NEW USER WHEN REGISTERED
 function generateUser(){
-    
+
 }
 
-//FUNCTION THAT 
+//FUNCTION THAT ADDS A NEW USER AS FRIEND
+function addCollaborator(){
+    
+}
 
 
 ?>

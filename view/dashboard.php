6414b75bdc8df10b1dbf97010aa3c1df4ec3c4bc
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-23 15:19:26 +0100

    changed email name

--- view/dashboard.php
+++ view/dashboard.php
@@ -27,7 +27,7 @@
             $database = connectDB($username, $password);
 
             //retrieve the user data from the DB
-            $data = array("email" => "David@kane.com");
+            $data = array("email" => "Thijs@kane.com");
             $result = readOneDB($database, "Users", $data);
 
             //generate the profile card
